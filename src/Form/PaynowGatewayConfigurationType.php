0e906b5688b74bec4ee6092961beae1585135f04
kuba.ceg <kuba.ceg@gmail.com>
2020-08-09 22:14:15 +0200

    First commit

---
+++ src/Form/PaynowGatewayConfigurationType.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @author Jakub Cegiełka <kuba.ceg@gmail.com>
+ */
+
+namespace SyliusPaynowPlugin\Form;
+
+use Paynow\Environment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class PaynowGatewayConfigurationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'environment',
+                ChoiceType::class,
+                [
+                    'choices' => [
+                        'kubaceg.paynow.production' => Environment::PRODUCTION,
+                        'kubaceg.paynow.sandbox' => Environment::SANDBOX,
+                    ],
+                    'label' => 'kubaceg.paynow.environment_type',
+                ]
+            )
+            ->add(
+                'api_key',
+                TextType::class,
+                [
+                    'label' => 'kubaceg.paynow.api_key',
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'kubaceg.paynow.gateway_configuration.api_key.not_blank',
+                                'groups' => ['sylius'],
+                            ]
+                        ),
+                    ],
+                ]
+            )
+            ->add(
+                'signature_key',
+                TextType::class,
+                [
+                    'label' => 'kubaceg.paynow.signature_key',
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'kubaceg.paynow.gateway_configuration.signature_key.not_blank',
+                                'groups' => ['sylius'],
+                            ]
+                        ),
+                    ],
+                ]
+            );
+    }
+}
---
+++ src/Model/PaynowApi.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @author Jakub Cegiełka <kuba.ceg@gmail.com>
+ */
+
+namespace SyliusPaynowPlugin\Model;
+
+final class PaynowApi
+{
+    /** @var string */
+    private $environment;
+
+    /** @var string */
+    private $apiKey;
+
+    /** @var string */
+    private $signatureKey;
+
+    /**
+     * @param string $environment
+     * @param string $apiKey
+     * @param string $signatureKey
+     */
+    public function __construct(string $environment, string $apiKey, string $signatureKey)
+    {
+        $this->environment = $environment;
+        $this->apiKey = $apiKey;
+        $this->signatureKey = $signatureKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEnvironment(): string
+    {
+        return $this->environment;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiKey(): string
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSignatureKey(): string
+    {
+        return $this->signatureKey;
+    }
+}
