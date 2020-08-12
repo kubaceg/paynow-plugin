d627daf3ddbc1ed3ce396d0fad65022be8b5515a
kuba.ceg <kuba.ceg@gmail.com>
2020-08-12 21:29:22 +0200

    plugin

---
+++ src/KubacegPaynowPlugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Jakub Cegiełka <kuba.ceg@gmail.com>
+ */
+
+namespace SyliusPaynowPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KubacegPaynowPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
