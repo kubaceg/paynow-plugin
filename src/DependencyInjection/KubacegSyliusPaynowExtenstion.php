f448313170c452fcbc2e570f5f4e8f589d3149fa
kuba.ceg <kuba.ceg@gmail.com>
2020-08-12 22:18:53 +0200

    DI

---
+++ src/DependencyInjection/KubacegSyliusPaynowExtenstion.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Jakub Cegiełka <kuba.ceg@gmail.com>
+ */
+
+namespace Kubaceg\SyliusPaynowPlugin\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+final class KubacegSyliusPaynowExtenstion extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
